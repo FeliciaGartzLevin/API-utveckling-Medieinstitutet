d6bdcae6a74fc930b519ad4f89cc93b273d2a725
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2023-01-10 14:55:08 +0100

    installed express and learning how to use GET and POST + check it in Postman

--- 02-express/server.js
+++ 02-express/server.js
@@ -9,6 +9,29 @@ const PORT = 3000
 // Create a new Express app
 const app = express()
 
+// GET /
+app.get('/', (req, res) => {
+	// res.send("Oh, hi there 😊")
+	res.send({
+		message: "Oh, hi there 😊",
+		lolcats: "Are funny",
+		reactions_on_isaks_memes: [
+			"rotflol",
+			"yolo"
+		],
+	})
+})
+
+// POST /
+app.post('/', (req, res) => {
+	res.send("I'm no mailbox 😡")
+})
+
+// GET /coffee
+app.get('/coffee', (req, res) => {
+	res.send("Is good for you!")
+})
+
 // Start listening for incoming requests on port 3000
 app.listen(PORT, () => {
 	console.log(`🥳 Yay, server started on localhost:${PORT}`)
