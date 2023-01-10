cb14eb6afcb52fcbc84675f4d5020b09084e0305
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2023-01-10 13:25:12 +0100

    first commit

---
+++ 01-basics/01-globals.js
@@ -0,0 +1,23 @@
+/***
+ * Globals
+ */
+
+/*
+setTimeout(() => {
+	console.log("I'll be back!");
+}, 3000);
+*/
+
+// console.log("Hello, world 🌱🌍!");
+
+/*
+// Will continue running until cancelled (Ctrl+C on both macOS and Windows)
+setInterval(() => {
+	console.log("Hello");
+}, 2000);
+*/
+
+console.log("Absolute path to directory for this file:", __dirname);
+console.log("Absolute path to this file:", __filename);
+
+console.log("☕️👀😋");
