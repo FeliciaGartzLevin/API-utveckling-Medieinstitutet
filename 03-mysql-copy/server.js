8a9d11831357122f06b1316b4f87d1cabec6e498
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2023-01-18 18:09:48 +0100

    workshop with SQL code

--- 03-mysql-copy/server.js
+++ 03-mysql-copy/server.js
@@ -3,10 +3,23 @@
  */
 
 // Require stuff
+require('dotenv').config()
 const express = require('express')
 const _ = require('lodash')
 const morgan = require('morgan')
 const PORT = 3000
+
+// Get the client
+const mysql = require('mysql2/promise')
+
+// Create connection to the database
+const connection = mysql.createConnection({
+	host: process.env.DATABASE_HOST,
+	port: process.env.DATABASE_PORT,
+	user: process.env.DATABASE_USER,
+	password: process.env.DATABASE_PASSWORD,
+	database: process.env.DATABASE_NAME,
+})
 
 // Create a new Express app
 const app = express()
@@ -23,6 +36,39 @@ app.get('/', (req, res) => {
 		message: "Oh, hi there ☺️",
 	})
 })
+
+
+app.get('/movies', async (req, res) => {
+	const db = await connection
+	const [rows] = await db.query('SELECT * FROM movies') //destructuring. plockar det som finns på array[0]
+	res.send(rows)
+})
+
+app.get('/movies/:movieId', async (req, res) => {
+	const movieId = Number(req.params.movieId) // kan även skrivas: const { movieId } = req.params  // same as `const movieId = req.params.movieId`
+
+	const db = await connection
+	const [movies] = await db.query(`SELECT * FROM movies WHERE id=?`, [movieId]) //frågetecken för att förebygga SQL Injection och t ex DROP (radera) hela databasen
+
+	// [movies] är destructuring. plockar det som finns på array[0]
+
+	// if id don't exist: send error message
+	if (!movies.length) {
+		res.status(404).send({ message: 'No such record exists.' })
+		return
+	}
+
+	res.send(movies[0])
+
+})
+
+// /**
+//  * @todo 1: Add route and logic for retrieving just one movie (ex: GET /movies/2)
+//  *
+//  * @todo 2: Handle if no movie with the requested id exists
+//  */
+
+
 
 // Catch requests where a route does not exist
 app.use((req, res) => {
